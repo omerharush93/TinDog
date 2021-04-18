d1600f123c9e9ea7d2449eb2687d1bfbc760d957
Tal <tal94franko@gmail.com>
2021-04-18 18:35:33 +0300

    added feeds fragment and profile fragment and viewpager

---
+++ app/src/main/java/com/example/tindog/views/FeedsFragment.java
@@ -0,0 +1,28 @@
+package com.example.tindog.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tindog.R;
+
+public class FeedsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feeds, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
--- app/src/main/java/com/example/tindog/views/MainActivity.java
+++ app/src/main/java/com/example/tindog/views/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.tindog.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.tindog.R;
+import com.example.tindog.adapters.TabAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 mviewPager;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             finish();
         }
-    }
 
-    public void signOut(View view) {
-        FirebaseAuth.getInstance().signOut();
-        recreate();
+        tabLayout = findViewById(R.id.main_bottom_nav);
+        mviewPager = findViewById(R.id.main_tabs_pager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), this.getLifecycle());
+        mviewPager.setAdapter(tabAdapter);
+        mviewPager.setOffscreenPageLimit(2);
+        int[] icons = {R.drawable.ic_baseline_home_24, R.drawable.ic_baseline_person_24};
+        new TabLayoutMediator(tabLayout, mviewPager,
+                (tab, position) -> tab.setIcon(icons[position])
+        ).attach();
+
     }
 }
