830a60931016b92bf1d89c3e8b8f5e2ca234708c
Tal <tal94franko@gmail.com>
2021-04-18 19:31:59 +0300

    started working on the recycler view

---
+++ app/src/main/java/com/example/tindog/interfaces/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.tindog.interfaces;
+
+import android.view.View;
+
+public interface OnItemClickListener {
+    void onItemClick(Object o, int position);
+}
--- app/src/main/java/com/example/tindog/views/FeedsFragment.java
+++ app/src/main/java/com/example/tindog/views/FeedsFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tindog.R;
+import com.example.tindog.adapters.FeedsRecyclerAdapter;
+import com.example.tindog.models.Dog;
+
+import java.util.ArrayList;
 
 public class FeedsFragment extends Fragment {
+
+    private RecyclerView feedsRecycler;
+    private FeedsRecyclerAdapter adapter;
 
 
     @Override
@@ -22,7 +31,20 @@ public class FeedsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        feedsRecycler = view.findViewById(R.id.feedsRecyclerView);
+        feedsRecycler.setHasFixedSize(true);
+        adapter = new FeedsRecyclerAdapter();
+        feedsRecycler.setAdapter(adapter);
+        adapter.setOnItemClickListener((o, position) -> {
+            Toast.makeText(getContext(), ((Dog) o).getAge() + "", Toast.LENGTH_SHORT).show();
+        });
+        ArrayList<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        adapter.setDogs(dogs);
 
     }
 }
